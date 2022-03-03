284674b10e5a1c97b9d5c2dd4417877c94b3d23b
labstraction <45163263+labstraction@users.noreply.github.com>
2022-03-03 13:24:31 +0100

    routing

---
+++ src/app/components/wrapper/subComponents/to-do-list/to-do-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/model/task';
+
+@Component({
+  selector: 'app-to-do-list',
+  templateUrl: './to-do-list.component.html',
+  styleUrls: ['./to-do-list.component.scss']
+})
+export class ToDoListComponent implements OnInit {
+
+  taskList: Task[];
+
+  constructor() {
+    let task1 = new Task("Studia javascript", 10);
+    let task2 = new Task("Studia Angular", 10);
+    let task3 = new Task("Studia Angular", 10);
+    this.taskList = [task1, task2, task3];
+  }
+
+  ngOnInit(): void {
+  }
+
+  taskDeleted(id: string){
+    let tempArray = [];
+    for (const task of this.taskList) {
+      if (task.id !== id) {
+        tempArray.push(task);
+      }
+    }
+    this.taskList = tempArray;
+  }
+
+}
--- src/app/model/task.ts
+++ src/app/model/task.ts
@@ -1,11 +1,27 @@
 export class Task {
+  id: string;
   name: string;
-  comment: string;
+  comment?: string;
+
+  tag?: string[];
 
   creationDate: Date;
-  doneDate: Date;
+  doneDate?: Date;
+  dueDate?: Date;
 
   priority: number;
-  repeat: number;
-  tag: string[];
+  repeat?: number;
+
+  constructor(name: string, priority: number = 0){
+    this.name = name;
+    this.priority = priority;
+    this.creationDate = new Date();
+    this.id = name.split(" ")[0] + Task.generateRandom();
+  }
+
+  static generateRandom(): number{
+    return Math.floor(Math.random() * 1000000);
+  }
+
 }
+
